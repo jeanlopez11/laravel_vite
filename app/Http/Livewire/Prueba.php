394dc690309b2a25841a974d64c966f3665c523b
unknown <jean1xd@gmail.com>
2023-03-06 08:59:30 -0500

    pruebas vite

---
+++ app/Http/Livewire/Prueba.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Prueba extends Component
+{
+    public $buscar;
+    public function render()
+    {
+        
+        return view('livewire.pruebaS.prueba');
+    }
+
+}
---
+++ resources/views/livewire/pruebas/prueba.blade.php
@@ -0,0 +1,7 @@
+<div>
+    {{-- The best athlete wants his opponent at his best. --}}
+    buscador
+    <input type="text" wire:model="buscar">
+    <h1> {{$buscar}}</h1>
+    <!-- <h2>jean lopez</h2> -->
+</div>
